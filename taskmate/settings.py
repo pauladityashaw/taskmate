8f8609d19a6399cc1084eb3f21e1cb472e90c584
pauladityashaw <pauladityashaw@gmail.com>
2021-01-17 13:56:14 +0530

    Updated requirements file

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CRIPSY_TEMPLATE_PACK = 'bootstrap5'
 
@@ -132,4 +133,4 @@ LOGIN_REDIRECT_URL = "todolist"
 
 LOGIN_URL = "login"
 
-django_heroku.settings(locals)
+django_heroku.settings(locals())
